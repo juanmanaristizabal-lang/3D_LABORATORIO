22df0f4d09ad70f0a5d6a6707d8ec1948d0a08a2
juanmaproxdxdxd <juanmanuelaristig@gmail.com>
2026-05-13 19:32:18 -0500

    s

--- 3D/Assets/Game/Scripts/MOVIMIENTO_ENEMIGO.cs
+++ 3D/Assets/Game/Scripts/MOVIMIENTO_ENEMIGO.cs
@@ -6,28 +6,55 @@ public class MOVIMIENTO_ENEMIGO : MonoBehaviour
 
     public Transform player;
     public NavMeshAgent agent;
-    public float rangoDeteccion = 10f; 
+    public float rangoDeteccion = 10f;
+
+    public Transform[] waypoints;
+    public float waypointDistance = 1f;
+    private int currentWaypoint = 0; 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[currentWaypoint].position);  
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float distancia = Vector3.Distance(transform.position, player.position);
+        //PLAYER SE ACERCA -> PERSEGUIR 
         if(distancia <= rangoDeteccion)
         {
-            agent.destination = player.position;
+            agent.SetDestination(player.position);
         }
+        //player lejos, patrullar
         else
         {
-            agent.ResetPath();
+            if(!agent.pathPending && agent.remainingDistance <= waypointDistance)
+            {
+                GoToNextWaypoint(); 
+            }
         }
     }
+
+    void GoToNextWaypoint()
+    {
+        currentWaypoint++;
+        if (currentWaypoint >= waypoints.Length)
+        {
+            currentWaypoint = 0;
+        }
+        agent.SetDestination(waypoints[currentWaypoint].position);
+    }
+
+
 
     void OnDrawGizmos()
     {
